a693541d58ef98a88fd3ada1b702065b5f8b17fc
Tata Naidu Nukala <tatanaidunukala@gmail.com>
2020-07-17 01:51:35 +0530

    prevel: home page intial commit globe and header setup draft
    Signed-off-by: Tata Naidu Nukala <tatanaidunukala@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <div className="row mainpage">
+
+            <div className="col-md-6">
+                    <div className="pulse"></div>
+            </div>
+                
+            <div className="col-md-6 description">
+                <div>
+                    <h1><span>Prevel</span> your Journey</h1>
+                </div>
+                <div>
+                    <h2>A site to <span>Pre</span>valuate your expenses. Predict your flight price here</h2>
+                </div>
+            </div>
+         </div>
+    </div>
+  );
+}
